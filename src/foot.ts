61131898ea7e632799bb760d137f84c8b025691f
Brandon seydel <brandonseydel@hotmail.com>
2020-03-25 22:19:30 -0500

    fix issues with sharing

--- src/foot.ts
+++ src/foot.ts
@@ -1,6 +1,11 @@
 export class Foot {
   location: Location;
+  title: string;
+  encodeURI: (uri: string) => string;
   constructor() {
     this.location = window.location;
+    this.title = document.title;
+    this.encodeURI = encodeURI;
+
   }
 }
