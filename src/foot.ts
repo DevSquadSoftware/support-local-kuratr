06ae519c908594a5705dfd869f6b876328871c87
Brandon seydel <brandonseydel@hotmail.com>
2020-03-25 22:26:03 -0500

    fix image url and strip lcoation for better sharing

--- src/foot.ts
+++ src/foot.ts
@@ -2,8 +2,10 @@ export class Foot {
   location: Location;
   title: string;
   encodeURI: (uri: string) => string;
+  href: string;
   constructor() {
     this.location = window.location;
+    this.href = window.location.href.split('?')[0]
     this.title = document.title;
     this.encodeURI = encodeURI;
 
